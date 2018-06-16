43c4fd32c92f386a79c8d011f9ac1c0e7487bb96
susupapa <miracleksh@gmail.com>
2018-06-16 09:32:22 +0000

    update #4

--- src/main/groovy/vw/demo/petclinic/interfaces/PetClinicController.groovy
+++ src/main/groovy/vw/demo/petclinic/interfaces/PetClinicController.groovy
@@ -23,6 +23,6 @@ class PetClinicController {
     
     @GetMapping("/developer")
     ResponseEntity getDeveloper(){
-        return ResponseEntity.ok("sungho_update_")
+        return ResponseEntity.ok("sungho_update__")
     }
 }
